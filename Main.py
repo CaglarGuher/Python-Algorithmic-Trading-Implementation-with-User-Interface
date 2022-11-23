0fda8b9822e34d7770ef3fa81a7dedceba5c19f7
Caglar Guher <88447267+CaglarGuher@users.noreply.github.com>
2022-11-23 15:37:04 +0300

    CHANGES

--- Main.py
+++ Main.py
@@ -1,3 +1,11 @@
+from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+import pandas as pd
+import json
+from datetime import datetime
+import seaborn as sns
+import mplfinance as mpf
+
+
 
 class Monitorizing:
     
@@ -6,10 +14,10 @@ class Monitorizing:
 
 
 #Logging
-
-
+## deeneme deneme
 
 class Account:
+    
     
     pass
 
@@ -25,16 +33,43 @@ class Asset_Operation():
 
 class Decision_Analysis():
 
-    
 
-    def ML_analysis():
-        pass
+    api_key = "API_KEY"
 
-    def Pop_analysis():
-        pass
-    
-    def grap_analysis():
-        pass
+    api_secret = "API_SECRET"
+
+    client = Client(api_key, api_secret)
+
+    def coin_info(coinname,interval,fromtime):
+        Client.KLINE_INTERVAL_1MONTH = "1m"
+        Client.KLINE_INTERVAL_1WEEK = "1w"
+        Client.KLINE_INTERVAL_1DAY = "1d"
+        Client.KLINE_INTERVAL_12HOUR = "12h"
+        Client.KLINE_INTERVAL_6HOUR = "6h"
+        Client.KLINE_INTERVAL_4HOUR = "4h"
+        Client.KLINE_INTERVAL_2HOUR = "2h"
+        Client.KLINE_INTERVAL_1HOUR = "1h"
+        Client.KLINE_INTERVAL_30MINUTE = "30min"
+        Client.KLINE_INTERVAL_15MINUTE ="15min"
+        Client.KLINE_INTERVAL_5MINUTE = "5min"
+        Client.KLINE_INTERVAL_3MINUTE = "3min"
+        Client.KLINE_INTERVAL_1MINUTE = "1min"
+        from_time = int(datetime.strptime(fromtime, "%Y-%m-%d").timestamp()*1000)
+        df =  pd.DataFrame(client.get_historical_klines(coinname, interval ,from_time))
+        df.columns = ["Opentime","Open","High","Low","Close","Volume","CloseTime","QuoteAssetTime","NumberOfTrades","VolumeOver24HourPeriod","PriceOver24HourPeriod ","Ignore"]
+        columns_to_float = ["Open","High","Low","Close","Volume","NumberOfTrades"]
+        df[columns_to_float] =  df[columns_to_float].astype(float)
+        df.drop(["Ignore","QuoteAssetTime","VolumeOver24HourPeriod","PriceOver24HourPeriod "],axis=1,inplace=True)
+        df["Opentime"] = pd.to_datetime(df["Opentime"]/1000,unit ="s")
+        df["CloseTime"] = pd.to_datetime(df["CloseTime"]/1000,unit ="s")
+
+        return df 
+    def current_price(coinname):
+        return client.get_margin_price_index(symbol='NEOUSDT')["price"]
+
+    def coin_visual(dataframe,a,b):
+        return mpf.plot(dataframe.set_index("CloseTime"),type = "line" ,style = "charles",mav =(a,b),volume = True)
+
 
     ##it will continue as researched##
      
